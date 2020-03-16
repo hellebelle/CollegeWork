777d360853d4c00d69b468ef4c9275087d40e842
Helana Maliakal <maliakaa@tcd.ie>
2020-03-16 13:26:18 +0000

    Updated assignments

--- 3rd Year/A.I/astar.pl
+++ 3rd Year/A.I/astar.pl
@@ -19,28 +19,27 @@ initKB(File) :- retractall(kb(_)), makeKB(File).
 astar(Node,Path,Cost) :- kb(KB), astar([[Node, [], 0 ]],Path,Cost,KB).
 
 astar([[Node, Path, Cost]|_], [Node, Path], Cost, _) :- goal(Node).
-astar([[Node, P, C]|Tail], Path, Cost, KB) :-   findall([X,[Node|P],Sum], (arc(Node, X, Y, KB), Sum is Y+C), Children),
-                                                addtofrontier(Children, Tail, Temp),
-                                                minSort(Temp, [[N1, P1, C1]|T1]),
-                                                astar([[N1,P1,C1]|T1], Path, Cost, KB).
-
-addtofrontier(Children,Frontier, NewFrontier) :- append (Children, Frontier, NewFrontier).
-
-minSort([Head|Tail], Result) :- sort(Head, [], Tail, Result).
-sort(Head, S, [], [Head|S]).
-sort(C, S, [Head|Tail], Result) :- lessthan(C, Head), !, sort(C, [Head|S], Tail, Result);   
-                                                         sort(Head, [C|S], Tail, Result).
-                                                        
-lessthan([Node1,_,Cost1|_],[Node2,_,Cost2|_]) :-heuristic(Node1, Hvalue1),
-                                                F1 is Cost1 + Hvalue1,
-                                                F2 is Cost2 + Hvalue2,
-                                                F1 =< F2.
+astar([[Node, P, C]|Rest], Path, Cost, KB) :- findall([X,[Node|P],Sum], (arc(Node, X, Y, KB), Sum is Y+C), Children),
+                                            addtofrontier(Children, Rest, Temp),
+                                            astar(Temp, Path, Cost, KB).
+goal([]).
+heuristic(Node,H) :- length(Node, H).
 
 arc([H|T], Node, Cost, KB) :-   member([H|B], KB),
                                 append(B, T, Node),
                                 length(B, L),
                                 Cost is L+1.
 
-heuristic(Node,H) :- length (Node, H).
+addtofrontier(Children,Frontier, NewFrontier) :- append(Children, Frontier, Temp), minSort(Temp, NewFrontier).
 
-goal([]).
+minSort([Head|Tail], Result) :- sort(Head, [], Tail, Result).
+sort(Head, S, [], [Head|S]).
+sort(C, S, [Head|Tail], Result) :- lessthan(C, Head), !, 
+                                   sort(C, [Head|S], Tail, Result);   
+                                   sort(Head, [C|S], Tail, Result).
+                                                        
+lessthan([Node1,_,Cost1|_],[Node2,_,Cost2|_]) :-heuristic(Node1, Hvalue1),
+                                                heuristic(Node2, Hvalue2),
+                                                F1 is Cost1 + Hvalue1,
+                                                F2 is Cost2 + Hvalue2,
+                                                F1 =< F2.
